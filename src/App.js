e88e7f17cd9fc94834b19dca995f1b3ba3715ab8
Darshana1907 <darshanakaur1907@gmail.com>
2024-08-06 20:42:58 +0530

    checked exam folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Comp from './Component/Comp';
+import Exam from './Component/Exam/Exam';
 
 function App() {
   return (
     <div >
      <Comp/>
+     <Exam/>
     </div>
   );
 }
