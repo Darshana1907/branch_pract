2f79f73ccea1262cd3180d0805bebf0ef2da7a81
Darshana1907 <darshanakaur1907@gmail.com>
2024-08-06 21:13:45 +0530

    changes

--- src/Component/Exam/Exam.jsx
+++ src/Component/Exam/Exam.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Exam = () => {
   return (
-    <div>Exam Comp
+    <div style={{color:"red", fontSize:"80px" , textAlign:"center",backgroundColor:"gray"}}>Exam Comp
       <h1>hii</h1>
+
     </div>
   )
 }
