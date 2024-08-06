d8ba63fea292eee7a90e76b4d5ff9260eba91da0
Darshana1907 <darshanakaur1907@gmail.com>
2024-08-06 20:32:02 +0530

    first file

---
+++ src/Component/Comp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Comp = () => {
+  return (
+    <div style={{fontSize :"80px", textAlign:"center" , color:"red" , fontWeight:"bold" }}>Comp</div>
+  )
+}
+
+export default Comp
